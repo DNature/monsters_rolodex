db57247c2476f374db1c7f25726b99f00b933ea6
osoka <victorosoka0@gmail.com>
2019-12-03 20:28:19 +0100

    Updates

---
+++ precache-manifest.732270f3829625473d47b1ebdd14656a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fcb4b4b3a29fc4236b5a415d0c7a4d2",
+    "url": "/monsters_rolodex/index.html"
+  },
+  {
+    "revision": "136363e8c2ba36f18520",
+    "url": "/monsters_rolodex/static/css/main.6205306c.chunk.css"
+  },
+  {
+    "revision": "e78965c9567adce4c0a8",
+    "url": "/monsters_rolodex/static/js/2.8e31940b.chunk.js"
+  },
+  {
+    "revision": "136363e8c2ba36f18520",
+    "url": "/monsters_rolodex/static/js/main.f9526478.chunk.js"
+  },
+  {
+    "revision": "16ff1cd2f6f22fa91c99",
+    "url": "/monsters_rolodex/static/js/runtime-main.733cd466.js"
+  }
+]);
